2e8356792fb29dad849f276b9e02dd52ec43a870
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2021-02-10 21:29:47 -0300

    preparando base do projeto

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
 
 import store from './store';
+import HomeTemp from './modules';
 
 export default function Index() {
   return (
     <Provider store={store}>
       <StatusBar style="auto" />
+      <HomeTemp />
     </Provider>
   );
 }
---
+++ src/modules/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const HomeTemp = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+      <Text style={{color: 'red', fontWeight: 'bold', fontSize: 24}} >
+        Preparando Projeto
+      </Text>
+    </View>
+  );
+}
+
+export default HomeTemp;
